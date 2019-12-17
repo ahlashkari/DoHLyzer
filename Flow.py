d4ec60b2e7501ab838dba2e40a8777c291954e42
Logan <ldavids2@unb.ca>
2019-12-17 09:02:23 -0400

    Add byte info to optional exclusion list.

--- Flow.py
+++ Flow.py
@@ -77,8 +77,9 @@ class Flow:
         parser = OptionParser()
         parser.add_option("-n","--no", dest="no")
         (options, args) = parser.parse_args()
+        data = {}
         if options.no == "header":
-            return {
+            data = {
                 # TLS information
                 # Hello exchange information
                 'ClientCipherSuit' : tls.client_cipher_suit(),
@@ -137,15 +138,15 @@ class Flow:
                 # 'DurationTotal' : packet_time.get_duration_total(),
 
                 # Information based and extrapolaited from the amount of bytes
-                # 'FlowBytesSent': barks.get_bytes_sent(),
-                # 'FlowSentRate': barks.get_sent_rate(),
-                # 'FlowBytesReceived': barks.get_bytes_received(),
-                # 'FlowReceivedRate': barks.get_received_rate(),
-                # 'ForwardHeaderBytes': barks.get_forward_header_bytes(),
-                # 'ForwardHeaderRate': barks.get_forward_rate(),
-                # 'ReverseHeaderBytes': barks.get_reverse_header_bytes(),
-                # 'ReverseHeaderRate': barks.get_reverse_rate(),
-                # 'HeaderInOutRatio' : barks.get_header_in_out_ratio(),
+                'FlowBytesSent': barks.get_bytes_sent(),
+                'FlowSentRate': barks.get_sent_rate(),
+                'FlowBytesReceived': barks.get_bytes_received(),
+                'FlowReceivedRate': barks.get_received_rate(),
+                'ForwardHeaderBytes': barks.get_forward_header_bytes(),
+                'ForwardHeaderRate': barks.get_forward_rate(),
+                'ReverseHeaderBytes': barks.get_reverse_header_bytes(),
+                'ReverseHeaderRate': barks.get_reverse_rate(),
+                'HeaderInOutRatio' : barks.get_header_in_out_ratio(),
                 'InitialTTL': barks.get_initial_ttl(),
 
                 # Statistical info extrapolaited and obtained from Packet lengths
@@ -170,14 +171,136 @@ class Flow:
                 'PacketTimeCoefficientofVariation': packet_time.get_cov(),
 
                 #Response times
-                'ResponseTimeTimeVariance': PacketTimeCoefficientofVariation.get_var(),
-                'ResponseTimeTimeStandardDeviation': PacketTimeCoefficientofVariation.get_std(),
-                'ResponseTimeTimeMean': PacketTimeCoefficientofVariation.get_mean(),
-                'ResponseTimeTimeMedian': PacketTimeCoefficientofVariation.get_median(),
-                'ResponseTimeTimeMode': PacketTimeCoefficientofVariation.get_mode(),
-                'ResponseTimeTimeSkewFromMedian': PacketTimeCoefficientofVariation.get_skew(),
-                'ResponseTimeTimeSkewFromMode': PacketTimeCoefficientofVariation.get_skew2(),
-                'ResponseTimeTimeCoefficientofVariation': PacketTimeCoefficientofVariation.get_cov(),
+                'ResponseTimeTimeVariance': packet_time.get_var(),
+                'ResponseTimeTimeStandardDeviation': packet_time.get_std(),
+                'ResponseTimeTimeMean': packet_time.get_mean(),
+                'ResponseTimeTimeMedian': packet_time.get_median(),
+                'ResponseTimeTimeMode': packet_time.get_mode(),
+                'ResponseTimeTimeSkewFromMedian': packet_time.get_skew(),
+                'ResponseTimeTimeSkewFromMode': packet_time.get_skew2(),
+                'ResponseTimeTimeCoefficientofVariation': packet_time.get_cov(),
+
+                # Information extrapolaited from the ip_addresses
+                # 'IsGoogle' : ip.is_google(),
+                # 'IsMalwareIP' : ip.is_bad(),
+
+                # Information about the packet flags
+                'FlagTotal': flags.get_flag_total(),
+                'NullFlagCount': flags.get_null_count(),
+                'PureFINCount': flags.get_fin_count(),
+                'EmbeddedFINCount': flags.get_emb_fin_count(),
+                'PureSYNCount': flags.get_syn_count(),
+                'EmbeddedSYNCount': flags.get_emb_syn_count(),
+                'PureRSTCount': flags.get_rst_count(),
+                'EmbeddedRSTCount': flags.get_emb_rst_count(),
+                'PurePSHCount': flags.get_psh_count(),
+                'EmbeddedPSHCount': flags.get_emb_psh_count(),
+                'PureACKCount': flags.get_ack_count(),
+                'EmbeddedACKCount': flags.get_emb_ack_count(),
+                'PureURGCount': flags.get_urg_count(),
+                'EmbeddedURGCount': flags.get_emb_urg_count(),
+                'PureECECount': flags.get_ece_count(),
+                'EmbeddedECECount': flags.get_emb_ece_count(),
+                'PureCWRCount': flags.get_cwr_count(),
+                'EmbeddedCWRCount': flags.get_emb_cwr_count(),
+                'RSTACKCount': flags.get_rstack_count(),
+                'SYNACKCount': flags.get_synack_count(),
+                'PushACKCount': flags.get_pshack_count(),
+                'SynFinCount': flags.get_synfin_count(),
+                'EmbeddedSynFin': flags.get_contain_finsyn_count(),
+            }
+        elif options.no == "bytes":
+            data = {
+            # Basic IP information
+                'SourceIP': self.src_ip,
+                'DestinationIP': self.dest_ip,
+                'SourcePort': self.src_port,
+                'DestinationPort': self.dest_port,
+
+                # TLS information
+                # Hello exchange information
+                'ClientCipherSuit' : tls.client_cipher_suit(),
+                'ClientHelloMessageLength' : tls.client_hello_msglen(),
+                'ServerHelloMessageLength' : tls.server_hello_msglen(),
+
+                'Compression' : tls.compression(),
+                'SessionLifetime' : tls.session_lifetime(),
+                #TLS extensions
+                #'AppData' : tls.app_data(),
+                'Alpn' : tls.alpn(),
+                # 'ClientAuthz' : tls.client_authz(),
+                # 'ClientCertificateType' : tls.client_cert_type(),
+                # 'ClientCertificateURL' : tls.client_cert_url(),
+                'Cookie' : tls.cookie(),
+                'Csr' : tls.csr(),
+                'EarlyData' : tls.early_data(),
+                # 'EarlyDataIndication' : tls.early_data_ind(),
+                # 'EarlyDataIndicationTicket' : tls.early_data_ind_ticket(),
+                # 'EncryptThenMac' : tls.encrypt_then_mac(),
+                'Heartbeat' : tls.heartbeat(),
+                'KeyShare' : tls.keyshare(),
+                'KeyShareCH' : tls.keyshare_ch(),
+                'MasterSecret' : tls.master_secret(),
+                'MaxFrag' : tls.max_frag(),
+                # 'OCSP' : tls.ocsp(),
+                'Padding' : tls.padding(),
+                'PostHandShakeAuth' : tls.post_handshake_auth(),
+                'PreSharedKey' : tls.pre_shared_key(),
+                'PskKeyExch' : tls.psk_key_exch(),
+                'RecordSizeLimit' : tls.record_size_limit(),
+                'Renegotiation' : tls.renegotiation(),
+                'ServerName' : tls.server_name(),
+                'SessionTicket' : tls.session_ticket(),
+                'SignatureAlgorithm' : tls.signature_algorithm(),
+                # 'SignatureAlgorithmCertificate' : tls.signature_algorithm_cert(),
+                'SupportedGroups' : tls.sup_groups(),
+                # 'SupportedEllipticCurves' : tls.supported_elip(),
+                'SupportedPointFormat' : tls.supported_point_format(),
+                'SupportedVersion' : tls.supported_version(),
+                'SupportedCh' : tls.supported_version_ch(),
+                # 'TicketEarlyDataInfo' : tls.ticket_early_data_info(),
+                # 'TLSAlert' : tls.tls_alert(),
+                # 'TruncatedHmac' : tls.truncated_hmac(),
+                # 'TrustedCA' : tls.trusted_ca(),
+                # 'UserMapping' : tls.user_mapping(),
+
+                # Information based on first 50 packets
+                'RelativeTimeList': packet_time.relative_time_list(),
+                'PacketSizeList': packet_length.first_fifty(),
+                'DirectionList': barks.direction_list(),
+
+                # Basic information from packet times
+                'TimeStamp': packet_time.get_time_stamp(),
+                'Duration': packet_time.get_duration(),
+
+                # Statistical info extrapolaited and obtained from Packet lengths
+                'PacketLengthVariance': packet_length.get_var(),
+                'PacketLengthStandardDeviation': packet_length.get_std(),
+                'PacketLengthMean': packet_length.get_mean(),
+                # 'PacketLengthGrandMean' : packet_length.get_grand_mean(),
+                'PacketLengthMedian': packet_length.get_median(),
+                'PacketLengthMode': packet_length.get_mode(),
+                'PacketLengthSkewFromMedian': packet_length.get_skew(),
+                'PacketLengthSkewFromMode': packet_length.get_skew2(),
+                'PacketLengthCoefficientofVariation': packet_length.get_cov(),
+
+                # Statistical info extrapolaited and obtained from Packet times
+                'PacketTimeVariance': packet_time.get_var(),
+                'PacketTimeStandardDeviation': packet_time.get_std(),
+                'PacketTimeMean': packet_time.get_mean(),
+                'PacketTimeMedian': packet_time.get_median(),
+                'PacketTimeMode': packet_time.get_mode(),
+                'PacketTimeSkewFromMedian': packet_time.get_skew(),
+                'PacketTimeSkewFromMode': packet_time.get_skew2(),
+                'PacketTimeCoefficientofVariation': packet_time.get_cov(),
+                'ResponseTimeTimeVariance': response.get_var(),
+                'ResponseTimeTimeStandardDeviation': response.get_std(),
+                'ResponseTimeTimeMean': response.get_mean(),
+                'ResponseTimeTimeMedian': response.get_median(),
+                'ResponseTimeTimeMode': response.get_mode(),
+                'ResponseTimeTimeSkewFromMedian': response.get_skew(),
+                'ResponseTimeTimeSkewFromMode': response.get_skew2(),
+                'ResponseTimeTimeCoefficientofVariation': response.get_cov(),
 
                 # Information extrapolaited from the ip_addresses
                 # 'IsGoogle' : ip.is_google(),
@@ -209,7 +332,7 @@ class Flow:
                 'EmbeddedSynFin': flags.get_contain_finsyn_count(),
             }
         elif options.no == "tcp_flags":
-            return {
+            data = {
                 # Basic IP information
                 'SourceIP': self.src_ip,
                 'DestinationIP': self.dest_ip,
@@ -307,21 +430,21 @@ class Flow:
                 'PacketTimeCoefficientofVariation': packet_time.get_cov(),
 
                 #Response Time
-                'ResponseTimeTimeVariance': PacketTimeCoefficientofVariation.get_var(),
-                'ResponseTimeTimeStandardDeviation': PacketTimeCoefficientofVariation.get_std(),
-                'ResponseTimeTimeMean': PacketTimeCoefficientofVariation.get_mean(),
-                'ResponseTimeTimeMedian': PacketTimeCoefficientofVariation.get_median(),
-                'ResponseTimeTimeMode': PacketTimeCoefficientofVariation.get_mode(),
-                'ResponseTimeTimeSkewFromMedian': PacketTimeCoefficientofVariation.get_skew(),
-                'ResponseTimeTimeSkewFromMode': PacketTimeCoefficientofVariation.get_skew2(),
-                'ResponseTimeTimeCoefficientofVariation': PacketTimeCoefficientofVariation.get_cov(),
+                'ResponseTimeTimeVariance': packet_time.get_var(),
+                'ResponseTimeTimeStandardDeviation': packet_time.get_std(),
+                'ResponseTimeTimeMean': packet_time.get_mean(),
+                'ResponseTimeTimeMedian': packet_time.get_median(),
+                'ResponseTimeTimeMode': packet_time.get_mode(),
+                'ResponseTimeTimeSkewFromMedian': packet_time.get_skew(),
+                'ResponseTimeTimeSkewFromMode': packet_time.get_skew2(),
+                'ResponseTimeTimeCoefficientofVariation': packet_time.get_cov(),
 
                 # Information extrapolaited from the ip_addresses
                 # 'IsGoogle' : ip.is_google(),
                 # 'IsMalwareIP' : ip.is_bad(),
             }
         elif options.no == "time": 
-            return {
+            data = {
                 'SourceIP': self.src_ip,
                 'DestinationIP': self.dest_ip,
                 'SourcePort': self.src_port,
@@ -408,14 +531,14 @@ class Flow:
                 'PacketLengthCoefficientofVariation': packet_length.get_cov(),
 
                 #Response time statistics
-                'ResponseTimeTimeVariance': PacketTimeCoefficientofVariation.get_var(),
-                'ResponseTimeTimeStandardDeviation': PacketTimeCoefficientofVariation.get_std(),
-                'ResponseTimeTimeMean': PacketTimeCoefficientofVariation.get_mean(),
-                'ResponseTimeTimeMedian': PacketTimeCoefficientofVariation.get_median(),
-                'ResponseTimeTimeMode': PacketTimeCoefficientofVariation.get_mode(),
-                'ResponseTimeTimeSkewFromMedian': PacketTimeCoefficientofVariation.get_skew(),
-                'ResponseTimeTimeSkewFromMode': PacketTimeCoefficientofVariation.get_skew2(),
-                'ResponseTimeTimeCoefficientofVariation': PacketTimeCoefficientofVariation.get_cov(),
+                'ResponseTimeTimeVariance': packet_time.get_var(),
+                'ResponseTimeTimeStandardDeviation': packet_time.get_std(),
+                'ResponseTimeTimeMean': packet_time.get_mean(),
+                'ResponseTimeTimeMedian': packet_time.get_median(),
+                'ResponseTimeTimeMode': packet_time.get_mode(),
+                'ResponseTimeTimeSkewFromMedian': packet_time.get_skew(),
+                'ResponseTimeTimeSkewFromMode': packet_time.get_skew2(),
+                'ResponseTimeTimeCoefficientofVariation': packet_time.get_cov(),
 
                 # Information extrapolaited from the ip_addresses
                 # 'IsGoogle' : ip.is_google(),
@@ -447,7 +570,7 @@ class Flow:
                 'EmbeddedSynFin': flags.get_contain_finsyn_count(),
         }
         elif options.no == "length":
-            return {
+            data = {
                 'SourceIP': self.src_ip,
                 'DestinationIP': self.dest_ip,
                 'SourcePort': self.src_port,
@@ -570,7 +693,7 @@ class Flow:
                 'EmbeddedSynFin': flags.get_contain_finsyn_count(),
         }
         elif options.no == "response":
-            return {
+            data = {
                 'SourceIP': self.src_ip,
                 'DestinationIP': self.dest_ip,
                 'SourcePort': self.src_port,
@@ -696,7 +819,7 @@ class Flow:
                 'EmbeddedSynFin': flags.get_contain_finsyn_count(),
         }
         else:
-            return {
+            data = {
                 # Basic IP information
                 'SourceIP': self.src_ip,
                 'DestinationIP': self.dest_ip,
@@ -832,17 +955,7 @@ class Flow:
                 'SynFinCount': flags.get_synfin_count(),
                 'EmbeddedSynFin': flags.get_contain_finsyn_count(),
             }
-
-        #else if the user chooses no header info
-
-        #else if tls flags excluded
-        #else if first 50 excluded
-        #else if packetlength info excluded
-        #else if packettime info
-        #else if ResponseTime info
-        #else if the user chooses no flag info
-        #else if the user chooses header bytes info added
-        #else
+        return data
 
     def add_packet(self, packet, direction) -> None:
         """Adds a packet to the current list of packets.
